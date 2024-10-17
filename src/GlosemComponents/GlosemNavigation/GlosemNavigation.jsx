3fda8c12a2db70bd3a8e4abeca8587426f04e2b8
webtesting22 <148945321+Sachin-Prepseed@users.noreply.github.com>
2024-10-17 17:57:44 +0530

    Some Changes

--- src/GlosemComponents/GlosemNavigation/GlosemNavigation.jsx
+++ src/GlosemComponents/GlosemNavigation/GlosemNavigation.jsx
@@ -30,7 +30,7 @@ const GlosemNavigation = () => {
         { label: "Home", path: "/" },
         { label: "About", path: "/glosemAbout" },
         { label: "Products", path: "/glosemProducts" },
-        { label: "Contact", path: "/contact" },
+        // { label: "Contact", path: "/contact" },
     ];
 
     return (
