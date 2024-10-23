9e129f4ab1c28a366af3c17c2ea36bae0ccc66fd
webtesting22 <webtesting2232@gmail.com>
2024-10-23 17:47:48 +0530

    Glosem

--- src/GlosemComponents/GlosemHome/GlosemHome.jsx
+++ src/GlosemComponents/GlosemHome/GlosemHome.jsx
@@ -63,9 +63,9 @@ const GlosemHome = () => {
                                     </div>
                                 </Col>
                                 <Col lg={12} md={24}>
-                                    <div>
-
-                                    </div>
+                                    {/* <div>
+                                        <img src="https://images.unsplash.com/photo-1675602488453-c3897a475af5?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                                    </div> */}
                                 </Col>
                             </Row>
                         </div>
